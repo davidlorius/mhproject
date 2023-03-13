dd7c89115217cc2a1ac5d22feb7b37f10fa347a5
David Leon <david@lorius.cloud>
2023-03-13 08:11:48 -0600

    Dockerize project

---
+++ init.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE mhproject_prod;
+CREATE DATABASE mhproject_test;
+GRANT ALL PRIVILEGES ON DATABASE mhproject_prod TO mhuser;
+GRANT ALL PRIVILEGES ON DATABASE mhproject_test TO mhuser;
+GRANT ALL PRIVILEGES ON DATABASE mhproject_development TO mhuser;
